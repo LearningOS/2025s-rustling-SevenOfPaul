9757e9377e779680f82ec7d6270228384e5e4945
paul <83870824+SevenOfPaul@users.noreply.github.com>
2025-03-24 13:34:07 +0000

    提交options1和quiz2

--- exercises/options/options1.rs
+++ exercises/options/options1.rs
@@ -13,7 +13,13 @@ fn maybe_icecream(time_of_day: u16) -> Option<u16> {
     // value of 0 The Option output should gracefully handle cases where
     // time_of_day > 23.
     // TODO: Complete the function body - remember to return an Option!
-    ???
+    if time_of_day==9{
+        Some(5)}else if time_of_day==10{
+             Some(5)}else if time_of_day==23{
+                Some(0)}else if time_of_day==22{
+                    Some(0)}else{
+                        None
+                    } 
 }
 
 #[cfg(test)]
